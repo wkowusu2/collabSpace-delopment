b3ddab027e5932ef61afd6a6e0729c1c1810ef66
William <tooknown27@gmail.com>
2025-10-29 16:34:35 +0000

    setup for developement has began

---
+++ collabSpace/src/main/java/com/collabspace/collabspace/CollabSpaceApplication.java
@@ -0,0 +1,14 @@
+package com.collabspace.collabspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabSpaceApplication.class, args);
+        System.out.println("CollabSpaceApplication started");
+    }
+
+}
---
+++ collabSpace/src/test/java/com/collabspace/collabspace/CollabSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabspace.collabspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
